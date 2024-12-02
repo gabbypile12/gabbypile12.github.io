c6666cfbc14c98fd7f15507d3e9733940640be99
gabbypile12 <gabbyp.447@gmail.com>
2024-12-02 09:08:38 -0500

    finally adding functionality to the navbar

--- personalWebsite/src/components/NavBar.jsx
+++ personalWebsite/src/components/NavBar.jsx
@@ -5,10 +5,10 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 function NavBar(){
 
     const [isMenuOpen, setMenuOpen] = useState(false);
-    function myFunction() {
-        alert("I am an alert box!");
-      }
-
+    function menu(){
+        setMenuOpen(!isMenuOpen);
+        alert(isMenuOpen);
+    }
     return(
         <div>
             <nav className = "bg-white flex items-center justify-between h-14 pr-8 pl-8 px-10">
@@ -19,7 +19,17 @@ function NavBar(){
                     <Link to = "/projects"><li className = "inline-block px-3 text-lg">Projects</li></Link>
                     <Link to = "/contact"><li className = "inline-block px-3 text-xl">Contact</li></Link>
                 </ul>
-                <div className = "block lg:hidden" onClick = {myFunction}><FontAwesomeIcon icon={faBars} /></div> 
+                <div className = "block lg:hidden" onClick = {menu}><FontAwesomeIcon icon={faBars} /></div> 
+               
+
+                {isMenuOpen && <div className=" absolute top-0 left-0 xl:hidden flex flex-col items-center gap-6 w-full bg-white mt-10">
+                    <ul className = " ">
+                    <Link to = "/"><li className = "w-full">Home</li></Link>
+                    <Link to = "/about"><li className = "w-full">About</li></Link>
+                    <Link to = "/projects"><li className = "w-full">Projects</li></Link>
+                    <Link to = "/contact"><li className = "w-full">Contact</li></Link>
+                    </ul>
+                </div>}
 
                 {/* <div className = "block lg:hidden" onClick = {() => setMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faBars} /></div> 
                 <div className = {`absolute top 24 left-0 w-full bg-white flex flex-col items-center gap-6 transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`}>
