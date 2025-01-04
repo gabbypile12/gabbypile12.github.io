63a6586dce7ab7e70c6be0122861837753f16381
Gabrielle  Pile <gabriellepile@Gabrielles-MacBook-Pro.local>
2025-01-04 11:06:19 -0800

    adding theabout page

--- personalWebsite/src/components/NavBar.jsx
+++ personalWebsite/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useState,useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import './NavBar.css'
+// import './NavBar.css'
 
 function NavBar(){
 
--- personalWebsite/src/pages/About.jsx
+++ personalWebsite/src/pages/About.jsx
@@ -1,10 +1,17 @@
 import NavBar from "../components/NavBar";
-import BiscuitGreen from "../images/"
+import BiscuitPurple from "../images/chocolateBiscuits.jpg";
+import BiscuitGreen from "../images/shirleybiscuitCoconut.png";
+import BiscuitRed from "../images/shirleybiscuitRed.png"
 function About(){
     return(
         <div className = "h-screen bg-purple">
             <NavBar></NavBar>
-            {/* <BiscuitGreen></BiscuitGreen> */}
+            <img src={BiscuitRed} />
+            <img src={BiscuitPurple}  />
+            <img src={BiscuitGreen} />
+            
+
+
             <p className = "text-white"> Gabrielle Pile is an asprining Software Engineer from Brooklyn, New York with a BA in Computer Science and a minor in African and Afican American Studies from Brandeis Univeristy. </p>
             <p className = "text-white">Passionate about using technology to improve lives, Gabrielle's work ranges from web development at the West African Research Center in Dakar, Senegal to projects like the Brandeis Black Student Archive.</p>
             <p className = "text-white">Inspired by her Haitain and Bajan hertiage and her Brooklyn upbringning, Gabrielle's colorful bodega-themed website celebrates her roots and reflects her ambitions</p>
